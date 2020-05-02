4a8bf67d5e1b26fff39bb5ef0af36331ba86b01b
Ramon Rangel Lima <ramon.lima@radixeng.com.br>
2020-05-01 21:00:31 -0300

    initial commit

---
+++ desafio-working-minds/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1>Desafio Working Minds</h1>
+  );
+}
+
+export default App;
---
+++ desafio-working-minds/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact />
+            </Switch>
+        </BrowserRouter>
+    );
+}
